486d23067a7244c8dfeca21aaff91573ae7c137b
Daniel Mattox <dmattox10@gmail.com>
2019-12-09 15:51:11 -0500

    MVP met

--- src/App.js
+++ src/App.js
@@ -10,10 +10,22 @@ import CardTwo from './components/CardTwo'
 
 function App() {
   const url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
-  const activeDateString = () => {
-    let dateString
-    const prepend = '&date='
-    return `${prepend}${dateString}`
+  const getISODate = () => {
+    const dateObj = new Date()
+    const month = dateObj.getUTCMonth() + 1
+    const day = dateObj.getUTCDate()
+    const year = dateObj.getUTCFullYear()
+    return `${year}-${month}-${day}`
+  }
+  const incrementDay = () => {
+    /*
+    if (moment() >= moment(activeDate, moment.ISO_8601).add(1, 'd')) {
+      updateDate(moment(activeDate, moment.ISO_8601).add(1, 'd'))
+    }
+    */
+  }
+  const decrementDay = () => {
+    //updateDate(moment(activeDate, moment.ISO_8601).subtract(1, 'd'))
   }
   const [apodState, updateApod] = useState({
     copyright: "Author / Copyright Holder",
@@ -23,20 +35,33 @@ function App() {
     title: 'Image Title',
     url: 'image url'
   })
-  const [activeDate, updateDate] = useState({
-
+  const [activeDate, updateDate] = useState(getISODate())
+  const [displayDate, updateDisplay] = useState({
+    day: "1",
+    month: "January"
   })
+  const prepend = '&date='
   useEffect(() => {
-    /*
-    axios.get(url + activeDateString())
+    
+    axios.get(url + prepend + activeDate)
       .then(res => updateApod(res.data))
-    */
-  }, [])
+    
+  }, [activeDate])
+  useEffect(() => {
+    const dateSplit = activeDate.toString().split('-')
+    const date = new Date(dateSplit[0], dateSplit[1] - 1 , dateSplit[2]) 
+    const month = date.toLocaleString('default', { month: 'long' })
+    const update = {
+      day: dateSplit[2],
+      month: month
+    }
+    updateDisplay(update)
+  }, [activeDate])
   return (
     <Router>
       <div className="App">
-        <div className="backing"><span class="s1">nasa</span><span class="s2">apod</span></div>
-        <Route exact path='/' render={(props) => <Card { ...props } url={ apodState.url } title={ apodState.title } copyright={ apodState.copyright } explanation={ apodState.explanation } media={ apodState.media_type } date={ apodState.date } />} />
+        <div className="backing"><span className="s1">nasa</span><span className="s2">apod</span></div>
+        <Route exact path='/' render={(props) => <Card { ...props } url={ apodState.url } title={ apodState.title } copyright={ apodState.copyright } explanation={ apodState.explanation } media={ apodState.media_type } day={ displayDate.day } month={ displayDate.month } decrementDay={ decrementDay } incrementDay={ incrementDay } />} />
         <Route exact path='/2' render={(props) => <CardTwo {...props} url={ apodState.url } title={ apodState.title } copyright={ apodState.copyright } explanation={ apodState.explanation } media={ apodState.media_type } date={ apodState.date } />} />
       </div>
     </Router>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import React from 'react'
-
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { Link } from 'react-router-dom'
+import { faStepBackward, faStepForward, faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import Thumbnail from './Thumbnail'
@@ -17,6 +17,11 @@ const Card = (props) => {
             <RightSide title={ props.title } author={ props.copyright } text={ props.explanation } />
             <h5>{ props.day }</h5>
             <h6>{ props.month }</h6>
+            <ul>
+                <li onClick={ props.decrementDay }><FontAwesomeIcon icon={ faStepBackward } /></li>
+                <li onClick={ props.incrementDay }><FontAwesomeIcon icon={ faStepForward } /></li>
+            </ul>
+            <div className='fab'><Link to='/2'><FontAwesomeIcon icon={ faArrowDown } /></Link></div>
         </div>
     )
 }
